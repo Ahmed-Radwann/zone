130dddc212eb28a24464137102962b274b7d78a5
Ahmed-Radwann <94936452+Ahmed-Radwann@users.noreply.github.com>
2023-11-16 01:41:41 +0200

    add location

---
+++ add_location/locationCubit/location_state.dart
@@ -0,0 +1,23 @@
+part of 'location_cubit.dart';
+
+@immutable
+abstract class LocationState {}
+
+class LocationInitial extends LocationState {}
+
+class LocationLoading extends LocationState {}
+
+class LocationSuccess extends LocationState {}
+
+
+class MarkerUpdate extends LocationState {}
+
+class AddCity extends LocationState {}
+
+class AddAdress extends LocationState {}
+
+class SendLoading extends LocationState {}
+
+class SendSuccess extends LocationState {}
+
+class SendError extends LocationState {}
